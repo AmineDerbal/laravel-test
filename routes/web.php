9b86e22deb79e35041cc7842c6cc93e004c0c88a
Amine derbal <aminedoran31@gmail.com>
2024-11-15 19:55:40 +0100

    Create routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/', 'index')->name('products.index');
+    Route::post('/', 'store')->name('products.store');
+
 });
